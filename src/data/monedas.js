9415c0b1851524a80acad50920ca24e6cef17e3a
Andrés Ruiz <jandresrios0@gmail.com>
2022-05-09 21:48:41 -0500

    Se crea un cotizador de criptomonedas consumiendo una API

---
+++ src/data/monedas.js
@@ -0,0 +1,9 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'MXN', nombre: 'Peso de Mexico'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'COP', nombre: 'Peso de Colombia'}
+]
+
+export { monedas }
